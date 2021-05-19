80dfacb82bb69503a67f097b1a71f0e9daa5d47e
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-05-19 14:55:39 -0500

    Redirect mismatched API routes to the frontend & allow for React Router to take control of routing

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as path from 'path';
 import apiRouter from './routes';
 
 const app = express();
@@ -6,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(apiRouter);
+
+app.use('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
